62a0d96c87c9b1c25db0c461f970eadd1a6358ec
hildelio <hildelio@gmail.com>
2023-07-13 20:18:55 -0300

    Projeto Trybe Futebol Clube :goal_net: :soccer: :runner: Req 01

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,45 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+import SequelizeMatch from './MatchModel';
+
+class SequelizeTeam extends
+  Model<InferAttributes<SequelizeTeam>, InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeam.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    timestamps: false,
+    tableName: 'teams',
+    modelName: 'Team',
+    underscored: true,
+  },
+);
+
+SequelizeTeam.hasMany(SequelizeMatch, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+SequelizeTeam.hasMany(SequelizeMatch, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+SequelizeMatch.belongsTo(SequelizeTeam, { foreignKey: 'homeTeamId', as: 'matchesHome' });
+SequelizeMatch.belongsTo(SequelizeTeam, { foreignKey: 'awayTeamId', as: 'matchesAway' });
+
+export default SequelizeTeam;
